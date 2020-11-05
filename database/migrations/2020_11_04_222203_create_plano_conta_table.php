77bffc15e14ff7b0ea13e364c0f097b88bee50dc
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-04 22:26:41 -0400

    adicionado migrate para tabela plano conta e relacionamentos

---
+++ database/migrations/2020_11_04_222203_create_plano_conta_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanoContaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plano_conta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome_conta')->nullable(false);
+            $table->date('data_pagamento')->nullable(false);
+            $table->bigInteger('fornecedor_id')->nullable(false);
+            $table->bigInteger('receita_id')->nullable(false);
+
+            $table->foreign('fornecedor_id')
+            ->references('id')->on('fornecedor')
+            ->onDelete('cascade');
+
+            $table->foreign('receita_id')
+            ->references('id')->on('receita')
+            ->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plano_conta');
+    }
+}
