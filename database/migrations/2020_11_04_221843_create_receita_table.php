6d5c6f8d927b8d0b4ff91f5f129fe985b7905b9b
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-04 22:21:04 -0400

    adicionado migrate para tabela receita e add relecionamento

---
+++ database/migrations/2020_11_04_221843_create_receita_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceitaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receita', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('descricao')->nullable(false);
+            $table->float('valor')->nullable(false);
+            $table->bigInteger('receita_id')->nullable(false);
+
+
+            $table->foreign('receita_id')
+                ->references('id')->on('receita')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receita');
+    }
+}
