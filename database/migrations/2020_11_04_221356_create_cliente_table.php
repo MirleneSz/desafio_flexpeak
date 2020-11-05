f37bade94f3cf020b47c18ebd5f941195bf63d12
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-04 22:18:00 -0400

    adicionado migrate para tabela cliente

---
+++ database/migrations/2020_11_04_221356_create_cliente_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome')->nullable(false);
+            $table->string('email')->unique();
+            $table->string('endereco')->nullable(false);
+            $table->string('telefone')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
