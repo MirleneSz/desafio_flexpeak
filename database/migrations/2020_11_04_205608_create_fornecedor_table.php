58780002e1287891557ae065b8ce85bfafad70e7
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-04 22:08:30 -0400

    first commit - estrutura do projeto

---
+++ database/migrations/2020_11_04_205608_create_fornecedor_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFornecedorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedor', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->string('nome')->nullable(false);
+            $table->string('endereco')->nullable(false);
+            $table->string('telefone')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fornecedor');
+    }
+}
