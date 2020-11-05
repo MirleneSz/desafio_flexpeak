c35cff8483639de7d8dc49a6a6b768447037e30d
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-04 23:40:19 -0400

    criando funções dentro do model Fornecedor e ligando ao Controller Fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\fornecedor\Fornecedor;
 
 class FornecedorController extends Controller
 {
-    //
+
+
+    public function index()
+    {
+        $fornecedor = new Fornecedor();
+        return view('home', ['fornecedores'=>$fornecedor]);
+
+
+    }
+
+    public function store(Request $request )
+    {
+
+    }
+
 }
