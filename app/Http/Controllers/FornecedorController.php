4100f84de9d9b36c17f710f121f228bf3cad67db
MirleneSz <mirlenedesouza2@gmail.com>
2020-11-05 01:28:43 -0400

    modificaçao index fornecedor controller

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,7 +10,10 @@ class FornecedorController extends Controller
 
     public function index()
     {
-        return view('fornecedor.index');
+
+        $fornecedor = new Fornecedor();
+        //  return ($fornecedor->all());
+        return view('fornecedor.index')->with(['fornecedores'=>$fornecedor->all()]);
     }
 
 
